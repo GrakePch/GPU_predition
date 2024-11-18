fda84b364b3ad760051741307f3788dc078f1a8e
GrakePCH <grakepch@gmail.com>
2024-11-18 01:36:28 -0500

    update

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ if len(sys.argv) < 2:
     
 filename = sys.argv[1]
 
-kernel = cfg(filename)
+
+kernel = cfg(filename)  # get the code block flow from PTX
 
 kernel.print()
+
+# Get from Hardware Manual
+max_wSM = 64   # Hardare limit for kepler+  it is 64
+
+# Get from Device Query
+S_zw = 32   # Warp size to schedule a set of threads
+
+ILP = 1 # assume or instruction are dependent. If independent, ILP could be higher.
+
+TLP = max_wSM   # For simpler model
