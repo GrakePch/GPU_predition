4b1bd96aa0a21de8ddcc4196b17bcf7f8f41dd03
GrakePCH <grakepch@gmail.com>
2024-11-18 22:46:20 -0500

    update bandwidth_test

--- bandwidth_test.cu
+++ bandwidth_test.cu
@@ -2,15 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CHECK_CUDA(call) do {                                   \
-    cudaError_t err = call;                                     \
-    if (err != cudaSuccess) {                                   \
-        fprintf(stderr, "CUDA error at %s:%d: %s\n",            \
-                __FILE__, __LINE__, cudaGetErrorString(err));   \
-        exit(EXIT_FAILURE);                                     \
-    }                                                           \
-} while (0)
-
 __global__ void memoryBandwidthKernel(float *dst, const float *src, size_t size) {
     size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < size) {
@@ -34,10 +25,10 @@ void testBandwidth(size_t size_in_mb) {
     }
 
     float *d_src, *d_dst;
-    CHECK_CUDA(cudaMalloc((void **)&d_src, size));
-    CHECK_CUDA(cudaMalloc((void **)&d_dst, size));
+    cudaMalloc((void **)&d_src, size);
+    cudaMalloc((void **)&d_dst, size);
 
-    CHECK_CUDA(cudaMemcpy(d_src, h_src, size, cudaMemcpyHostToDevice));
+    cudaMemcpy(d_src, h_src, size, cudaMemcpyHostToDevice);
 
     int threads_per_block = 256;
     int blocks_per_grid = (num_elements + threads_per_block - 1) / threads_per_block;
@@ -45,32 +36,30 @@ void testBandwidth(size_t size_in_mb) {
     // Warm-up run
     // To avoid initialization overhead, and get some cache.
     memoryBandwidthKernel<<<blocks_per_grid, threads_per_block>>>(d_dst, d_src, num_elements);
-    CHECK_CUDA(cudaDeviceSynchronize());
+    cudaDeviceSynchronize();
 
-    // Measure kernel execution time
     cudaEvent_t start, stop;
-    CHECK_CUDA(cudaEventCreate(&start));
-    CHECK_CUDA(cudaEventCreate(&stop));
-    CHECK_CUDA(cudaEventRecord(start, 0));
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
 
     memoryBandwidthKernel<<<blocks_per_grid, threads_per_block>>>(d_dst, d_src, num_elements);
 
-    CHECK_CUDA(cudaEventRecord(stop, 0));
-    CHECK_CUDA(cudaEventSynchronize(stop));
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
 
     float elapsed_ms = 0.0f;
-    CHECK_CUDA(cudaEventElapsedTime(&elapsed_ms, start, stop));
+    cudaEventElapsedTime(&elapsed_ms, start, stop);
 
-    // Calculate bandwidth
     float bandwidth = (2.0f * size) / (elapsed_ms * 1e6); // GB/s
     printf("Bandwidth: %.2f GB/s\n", bandwidth);
 
     free(h_src);
     free(h_dst);
-    CHECK_CUDA(cudaFree(d_src));
-    CHECK_CUDA(cudaFree(d_dst));
-    CHECK_CUDA(cudaEventDestroy(start));
-    CHECK_CUDA(cudaEventDestroy(stop));
+    cudaFree(d_src);
+    cudaFree(d_dst);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
 }
 
 int main() {
